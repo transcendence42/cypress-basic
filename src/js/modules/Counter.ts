9e1c44609228be8ef54e092d2e1a0770b8af8e7c
hochan Lee <hochan049@gmail.com>
2021-05-29 17:39:00 +0900

    feat: input 태크 max min attribute 추가 + 버튼을 눌렀을 때 count가 12가 넘는 경우 더이상 증가하지 못한다. (Max 값이 12) - 버튼을 눌렀을 때 count는 8보다 작아지는 경우 감소하지 못한다. (Min 값이 8) 기능 구현 완료

--- src/js/modules/Counter.ts
+++ src/js/modules/Counter.ts
@@ -8,7 +8,7 @@ export default function Counter( {$app}: {$app: HTMLDivElement | null} ):void {
           <h1>ui counter</h1>
           <div class="counter">
             <a href="#" class="minus-button"><span>-</span></a>
-            <input name="count" type="text" class="count-display" value="10">
+            <input name="count" type="text" class="count-display" value="10" max="12" min="8">
             <a href="#" class="plus-button"><span>+</span></a>
           </div>
         </div>`;
