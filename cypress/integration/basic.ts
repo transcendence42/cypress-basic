8841f6755dac319adcbdf18af0292fbb5c1bac26
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-06-01 10:24:49 +0900

    feat: add cypress test

--- cypress/integration/basic.ts
+++ cypress/integration/basic.ts
@@ -1,6 +1,22 @@
-it("should perform basic google search", () => {
-  cy.visit("https://google.com");
-  cy.get('[name="q"]')
-    .type("subscribe")
-    .type("{enter}");
+it("should perform counter program", () => {
+  cy.visit("http://localhost:8080");
+  cy.get(".count-display").should("have.value", "10");
+
+  cy.get(".plus-button").click();
+  cy.get(".count-display").should("have.value", "11");
+  cy.get(".plus-button").click();
+  cy.get(".count-display").should("have.value", "12");
+  cy.get(".plus-button").click();
+  cy.get(".count-display").should("have.value", "12");
+
+  cy.get(".minus-button").click();
+  cy.get(".count-display").should("have.value", "11");
+  cy.get(".minus-button").click();
+  cy.get(".count-display").should("have.value", "10");
+  cy.get(".minus-button").click();
+  cy.get(".count-display").should("have.value", "9");
+  cy.get(".minus-button").click();
+  cy.get(".count-display").should("have.value", "8");
+  cy.get(".minus-button").click();
+  cy.get(".count-display").should("have.value", "8");
 });
